5a177315977374a3b7a3112f20e9924dd874ae19
Jackson Lam <03jackson.lam@kamsc.org>
2025-09-17 13:53:59 -0400

    yeh

--- bankaccount.py
+++ bankaccount.py
@@ -1,5 +1,26 @@
 # TODO create class BankAccount
 
-if __name__ == "__main__":
     # create BankAccount below this
-    pass
+class BankAccount:
+    def __init__(self,balance):
+        self.balance = balance
+    def get_balance(self):
+        self.balance
+        return self.balance
+    def deposit(self,amnt):
+        self.balance
+        if amnt < 0:
+            return self.balance
+        else:
+            balance += amnt
+            return self.balance
+    def withdraw(self ,amnt)
+        self.balance
+        if amnt > 0:
+            return self.balance
+        else:
+            if balance-amnt<0:
+                return "Insufficient Funds"
+            else:
+                balance -= amnt
+                return self.balance
