0d5474e56379c0e13fe920cd2b50d2d8755c9360
Jackson Lam <03jackson.lam@kamsc.org>
2025-09-19 13:05:18 -0400

    Update bankaccount.py

--- bankaccount.py
+++ bankaccount.py
@@ -2,24 +2,24 @@
 
     # create BankAccount below this
 class BankAccount:
-    def __init__(self,balance):
+    def __init__(self,balance=0):
         self.balance = balance
     def get_balance(self):
         self.balance
         return self.balance
-    def deposit(self,amnt):
+    def deposit(self,dep):
         self.balance
-        if amnt < 0:
+        if dep < 0:
             return self.balance
         else:
-            balance += amnt
+            self.balance += dep
             return self.balance
     def withdraw(self ,amnt):
         self.balance
         if amnt > 0:
             return self.balance
         else:
-            if balance-amnt<0:
+            if self.balance-amnt<0:
                 return "Insufficient Funds"
             else:
                 balance -= amnt
