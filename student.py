d4865de306dd28411b38d80dbfac8285d3501f96
Jackson Lam <03jackson.lam@kamsc.org>
2025-09-22 12:37:09 -0400

    Update student.py

--- student.py
+++ student.py
@@ -2,12 +2,12 @@ class Student:
     def __init(self, name, grade):
         self.name = name
         self.grade = grade
-    def get_name(name):
+    def get_name(self):
         self.name
         return self.name
-    def get_grade(grade):
+    def get_grade(self):
         self.grade
         return self.grade
-    def set_grade(grade, newgrade):
+    def set_grade(self, newgrade):
         self.grade = newgrade
         return self.grade
