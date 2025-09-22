1e93239c6564fb7fd1e6019a338b73a0c341932b
Jackson Lam <03jackson.lam@kamsc.org>
2025-09-22 12:42:50 -0400

    Update pythagoreantheorem.py

--- pythagoreantheorem.py
+++ pythagoreantheorem.py
@@ -1,5 +1,7 @@
-# TODO create class PythagoreanTheorem
-
-if __name__ == "__main__":
-    # create PythagoreanTheorem below this
-    pass
+import math
+class PythagoreanTheorem:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def hypotenuse(self)
+        return math.sqrt(a**2+b**2)
