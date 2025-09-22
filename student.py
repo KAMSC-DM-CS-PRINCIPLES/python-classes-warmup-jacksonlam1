6aa4c15cb5395109b2a03b682811277c24fa2588
Jackson Lam <03jackson.lam@kamsc.org>
2025-09-22 12:35:02 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,5 +1,13 @@
-# TODO create class Student
-
-if __name__ == "__main__":
-    # create Student below this
-    pass
+class student:
+    def __init(self, name, grade):
+        self.name = name
+        self.grade = grade
+    def get_name(name):
+        self.name
+        return self.name
+    def get_grade(grade):
+        self.grade
+        return self.grade
+    def set_grade(grade, newgrade):
+        self.grade = newgrade
+        return self.grade
