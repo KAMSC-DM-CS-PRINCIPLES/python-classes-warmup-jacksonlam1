83e7b468cc2d0bbfd41ca093b5a52e4f76b59bd0
Jackson Lam <03jackson.lam@kamsc.org>
2025-09-22 12:43:40 -0400

    Update pythagoreantheorem.py

--- pythagoreantheorem.py
+++ pythagoreantheorem.py
@@ -3,5 +3,5 @@ class PythagoreanTheorem:
     def __init__(self, a, b):
         self.a = a
         self.b = b
-    def hypotenuse(self)
+    def hypotenuse(self):
         return math.sqrt(a**2+b**2)
